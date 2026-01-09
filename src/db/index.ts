3e0274886d03d50ef3970fbf2535f1db5527e675
AyhamDev <ayhamgm81@gmail.com>
2026-01-09 19:04:33 +0200

    hot fixes

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,8 +3,8 @@ const connectDB = async (defualtAdmin = true) => {
   try {
     if (Bun.env.MONGO_URI !== undefined) {
       const conn = await mongoose.connect(Bun.env.MONGO_URI, {
-        minPoolSize: 10,
-        maxPoolSize: 500,
+        minPoolSize: 0,
+        maxIdleTimeMS: 10000, // Closes idle connections after 10 seconds
       });
       return null;
     }
